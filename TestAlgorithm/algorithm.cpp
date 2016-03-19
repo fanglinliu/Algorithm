83ccbbf98fbb40fcf7cbb60d4a314f133aec7174
Fanglin Liu <fanglliu@cisco.com>
2016-03-19 15:19:22 +0800

    Add TestAlgorithm Project

---
+++ TestAlgorithm/algorithm.cpp
@@ -0,0 +1,9 @@
+//
+//  algorithm.cpp
+//  TestAlgorithm
+//
+//  Created by fanglliu on 3/19/16.
+//  Copyright © 2016 fanglliu. All rights reserved.
+//
+
+#include "algorithm.hpp"
---
+++ TestAlgorithm/algorithm.hpp
@@ -0,0 +1,14 @@
+//
+//  algorithm.hpp
+//  TestAlgorithm
+//
+//  Created by fanglliu on 3/19/16.
+//  Copyright © 2016 fanglliu. All rights reserved.
+//
+
+#ifndef algorithm_hpp
+#define algorithm_hpp
+
+#include <stdio.h>
+
+#endif /* algorithm_hpp */
